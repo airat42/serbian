5f1dfa1820630158004ff56d33d4579e5c9f6363
airat42 <khalilushka42@gmail.com>
2022-11-20 15:03:24 +0300

    Global change

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html', {'title': 'Главная страница'})
 
-class Home(ListWiew):
-    model = Dictionary
-    template_name = 'main/index.html'
+# class Home(ListWiew):
+#     model = Dictionary
+#     template_name = 'main/index.html'
     
