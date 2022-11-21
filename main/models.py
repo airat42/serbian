1fa9edf96fd32e6fa275daa61a37a5987ab5f6a5
airat42 <khalilushka42@gmail.com>
2022-11-22 01:52:14 +0300

    ListView

--- main/models.py
+++ main/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Dictionary(models.Model):
     russian = models.CharField(max_length=255)
     serbian = models.CharField(max_length=255)
+    def __str__(self):
+        return self.russian
--- main/views.py
+++ main/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from main.models import Dictionary
+import random
 
-def index(request):
-    return render(request, 'main/index.html', {'title': 'Главная страница'})
+rand_int = random.randint(0, len(Dictionary.objects.all()))
+queryset1 = Dictionary.objects.raw(f'SELECT * FROM main_dictionary WHERE id = {rand_int}')
 
-# class Home(ListWiew):
-#     model = Dictionary
-#     template_name = 'main/index.html'
+class Home(ListView):
+    model = Dictionary
+    template_name = 'main/index.html'
+
+    def get_queryset(self):
+        queryset = list(Dictionary.objects.all().values('russian', 'serbian'))[0]
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['queryset1'] = queryset1
+        return context
     
